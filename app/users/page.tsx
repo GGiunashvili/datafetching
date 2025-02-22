f8726bcbdc34f00e466c3fdff1332106d765cb3b
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-02-22 23:40:55 +0400

    add LOADING

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,20 +1,27 @@
+import { Suspense } from "react";
+
+import Loading from "./loading";
+
 export default async function Page() {
+  // ემულირება: 2 წამის ლოდინი
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   const response = await fetch("http://localhost:5000/users", {
-    cache: "no-cache",
+    cache: "no-store", // ან "no-cache"
   });
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
+
   const users = await response.json();
 
   return (
-    <div>
-      {users.map((user: any) => (
-        <div key={user.id}>
-          <h1>{user.name}</h1>
-          <h1>{user.id}</h1>
-        </div>
-      ))}
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div>
+        {users.map((user: any) => (
+          <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h1>{user.id}</h1>
+          </div>
+        ))}
+      </div>
+    </Suspense>
   );
 }
